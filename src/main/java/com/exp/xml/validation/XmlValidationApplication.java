282408bb23ad1bc7c373c831b6e30ca06a191983
Christian Braune <christian.braune@creatision.de>
2025-06-07 11:27:09 +0200

    Initial commit

---
+++ src/main/java/com/exp/xml/validation/XmlValidationApplication.java
@@ -0,0 +1,13 @@
+package com.exp.xml.validation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmlValidationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exp/xml/validation/XmlValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exp.xml.validation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmlValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
